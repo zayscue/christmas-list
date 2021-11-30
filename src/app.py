75b31297c7b5f5316898eeba04690f2fe8cea8fa
Zackery Ayscue <ztayscue@gmail.com>
2021-11-30 15:48:10 -0500

    Added get christmas list endpoint

--- src/app.py
+++ src/app.py
@@ -13,6 +13,8 @@ logging.getLogger().setLevel(logging.INFO)
 db = boto3.resource('dynamodb')
 app = Flask(__name__)
 
+TABLE_NAME = 'ChristmasLists'
+
 @app.route('/')
 def index():
     return jsonify(status=200, message='OK')
@@ -20,12 +22,21 @@ def index():
 @app.route('/api/christmas-list', methods=['POST'])
 def create_list():
     data = request.json
-    data['id'] = str(uuid.uuid4())
-    data['created_at'] = str(datetime.utcnow())
-    table = db.Table('ChristmasLists')
-    table.put_item(Item=data)
+    item = {}
+    if 'name' in data:
+        item['name'] = data['name']
+    item['id'] = str(uuid.uuid4())
+    item['created_at'] = str(datetime.utcnow())
+    table = db.Table(TABLE_NAME)
+    table.put_item(Item=item)
     app.logger.info('created new christmas list')
-    return jsonify(data)
+    return jsonify(item)
+
+@app.route('/api/christmas-list/<id>', methods=['GET'])
+def get_list(id):
+    table = db.Table(TABLE_NAME)
+    item = table.get_item(Key={'id': id})
+    return jsonify(item['Item'])
 
 def lambda_handler(event, context):
     return awsgi.response(app, event, context, base64_content_types={"image/png"})
